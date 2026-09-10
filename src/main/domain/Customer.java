f7c7f64cbeb4237c226fe3b8238cc012d2efbb71
Leandrafrancine <166594282+Leandrafrancine@users.noreply.github.com>
2026-09-10 17:24:34 -0400

    modification de id(variable) en constante

--- src/main/domain/Customer.java
+++ src/main/domain/Customer.java
@@ -1,7 +1,7 @@
 package main.domain;
 
 public class Customer {
-    private int id;
+    private final int id;
     private String name;
     private int loyaltyYears;
     private boolean active;
@@ -17,7 +17,7 @@ public class Customer {
         this.accountBalance = accountBalance;
     }
 
-    
+
     public String getName() { return name; }
     public int getLoyaltyYears() { return loyaltyYears; }
     public boolean isActive() { return active; }
